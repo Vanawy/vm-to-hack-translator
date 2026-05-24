32fd4383f309832f6a4e795d8ed0524ef4b046d1
Ilya Sakharchuk <saharchuk999@gmail.com>
2026-05-24 19:19:46 +0300

    add error output

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use crate::command::Command;
 pub use crate::translator::Translator;
 use regex::Regex;
+use std::process::exit;
 
 mod command;
 mod segment;
@@ -9,16 +10,22 @@ mod translator;
 pub fn translate(filename: String, input: String) -> String {
     let re = Regex::new(r"\s+").expect("Can't parse regex");
 
-    let mut translator = Translator::new(filename);
+    let mut translator = Translator::new(filename.clone());
 
     let lines: Vec<String> = input
         .lines()
-        .map(|s| s.trim().to_lowercase())
-        .map(|s| re.replace_all(s.as_str(), " ").to_string())
-        .filter(|s| !(s.is_empty() || s.starts_with("//")))
-        .map(|s| s.parse::<Command>())
-        .filter(|c| c.is_ok())
-        .map(|c| c.unwrap())
+        .enumerate()
+        .map(|(n, s)| (n, s.trim().to_lowercase()))
+        .map(|(n, s)| (n, re.replace_all(s.as_str(), " ").to_string()))
+        .filter(|(_, s)| !(s.is_empty() || s.starts_with("//")))
+        .map(|(n, s)| (n, s.parse::<Command>()))
+        .map(|(n, c)| match c {
+            Ok(c) => c,
+            Err(err) => {
+                eprintln!("{:?}: {}:{}:1", err, filename.clone(), n + 1);
+                exit(1);
+            }
+        })
         .flat_map(|c| translator.code(c))
         .collect();
 
